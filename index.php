61074dead47dadd935e786a0b8f07dfee59c72bb
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-02-28 23:55:05 +0600

    new

--- index.php
+++ index.php
@@ -1,26 +1,75 @@
 <?php require("header.php");?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-lg-3">
-            <div class="card">
-                <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
+                <form action="manage_cart.php" method="post">
+                    <div class="card">
+                        <img class="card-img-top" src="image/1.jpg" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">food 1</h5>
+                            <p class="card-text">price 120</p>
+                            <button type="submit" name='ADD_to_cart' class="btn btn-info"> Add to card</button>
+                            <input type="hidden" name="item_name" value="food 1">
+                            <input type="hidden" name="price" value="120">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-3">
+                <form action="manage_cart.php" method="post">
+                    <div class="card">
+                        <img class="card-img-top" src="image/1.jpg" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">food 2</h5>
+                            <p class="card-text">price 150</p>
+                            <button type="submit" name='ADD_to_cart'class="btn btn-info"> Add to card</button>
+                            <input type="hidden" name="item_name" value="food 2">
+                            <input type="hidden" name="price" value="150">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-3">
+                <form action="manage_cart.php" method="post">
+                    <div class="card">
+                        <img class="card-img-top" src="image/1.jpg" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">food 3</h5>
+                            <p class="card-text">price 200</p>
+                            <button type="submit" name='ADD_to_cart' class="btn btn-info"> Add to card</button>
+                            <input type="hidden" name="item_name" value="food 3">
+                            <input type="hidden" name="price" value="200">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-3">
+                <form action="manage_cart.php" method="post">
+                    <div class="card">
+                        <img class="card-img-top" src="image/1.jpg" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">food 4</h5>
+                            <p class="card-text">price 120</p>
+                            <button type="submit" name='ADD_to_cart' class="btn btn-info"> Add to card</button>
+                            <input type="hidden" name="item_name" value="food 4">
+                            <input type="hidden" name="price" value="250">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
---
+++ manage_cart.php
@@ -0,0 +1,11 @@
+<?php 
+if($_SERVER["REQUEST_METHOD"]){
+if(isset($_POST['ADD_to_cart'])){
+
+    echo'name';
+}
+
+}
+
+
+?>
