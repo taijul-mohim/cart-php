7bd424c2264d50127483d75549a72dc92470b39c
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-03-19 20:50:12 +0600

    done

--- mycart.php
+++ mycart.php
@@ -8,51 +8,55 @@ session_start()
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>my cart</title>
 </head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 text-center border rounded my-5">
-            <h1>My Cart</h1>
-            <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
 
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center border rounded my-5">
+                <h1>My Cart</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Serial name</th>
+                            <th scope="col">Item name</th>
+                            <th scope="col">Item price</th>
+                            <th scope="col">Quentity</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     </div>
-</div>
-    
+
 </body>
+
 </html>
